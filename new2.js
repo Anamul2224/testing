8e4b03b21593767002caa706564ff90aceaf4b64
Anamul2224 <Anamulhaque2224@gmail.com>
2022-11-24 22:14:24 +0600

    this is another git

---
+++ new2.js
@@ -0,0 +1,17 @@
+let nnn = "anamul, robi, kubra khan, silly";
+let sad = nnn.slice(-18, -7);
+
+console.log(sad);
+console.log(nnn.length);
+
+let str = "Apple, Banana, Kiwi";
+let part = str.substr(7,6);
+console.log(part);
+
+function rest(){
+  let text = "there is only  god true lord of the world";
+  console.log(text.replace( "god", "Allah"));
+};
+
+let rs = "I love rasul and rasul";
+console.log(rs.replace("rasul", "Allah"));
